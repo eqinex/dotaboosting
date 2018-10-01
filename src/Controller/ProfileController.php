83249fd64b1900a9cf313949a47de9de0ee6463f
AT\MazitovTR <timur.mazitov@npo-at.com>
2018-10-01 18:03:09 +0500

    create registration and fix style autorization, but need fix trait

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MazitovTR
+ * Date: 01.10.2018
+ * Time: 12:30
+ */
+
+namespace App\Controller;
+
+
+class ProfileController
+{
+
+}
---
+++ src/Repository/RepositoryAwareTrait.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MazitovTR
+ * Date: 01.10.2018
+ * Time: 12:36
+ */
+
+namespace App\Repository;
+
+
+class RepositoryAwareTrait
+{
+
+}
